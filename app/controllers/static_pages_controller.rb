6efd8b13b331225a55f36207d48b038620983451
Subodh <subodh186@gmail.com>
2015-03-06 02:45:22 +0000

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    @micropost = current_user.microposts.build if logged_in?
+    @feed_items = current_user.feed.paginate(page: params[:page])
+
   end
   
   def about
@@ -10,6 +13,5 @@ class StaticPagesController < ApplicationController
   
   def contact
   end
-  
   
 end
